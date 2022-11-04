1d6141d39b6a70d0b306074c75c5e56ac9bc2d28
bertokattila <bertok.attila@ardinsys.eu>
2022-11-04 20:27:01 +0100

    ui

--- ui.py
+++ ui.py
@@ -13,6 +13,9 @@ class UI(object):
     filename = ''
     frame = tk.Frame(master=window, width=550, height=250)
     frame.pack()
+    frame_a = tk.Frame()
+    label_license = tk.Label(master=frame_a, text="")
+    label_license.pack()
 
     def openfn(self):
         filename = filedialog.askopenfilename(title='open')
@@ -21,6 +24,7 @@ class UI(object):
     def open_img(self):
         for widget in self.frame.winfo_children():
             widget.destroy()
+        self.label_license.config(text="")
         self.filename = self.openfn()
         img = Image.open(self.filename)
         img.thumbnail((500, 500), Image.ANTIALIAS)
@@ -42,21 +46,21 @@ class UI(object):
         print(self.filename)
         res = get_license_plate(self.filename)
 
-        print(res["plate_options"])
+        self.label_license.config(text=res["plate_options"])
         self.display_img(Image.fromarray(np.uint8(res["image"])).convert('RGB'))
 
     def __init__(self):
-        frame_a = tk.Frame()
-        label_license = tk.Label(master=frame_a, text="")
+
+        label_license = tk.Label(master=self.frame_a, text="")
         label_license.pack()
-        label_a = tk.Button(master=frame_a, command=self.open_img, text="Load image")
+        label_a = tk.Button(master=self.frame_a, command=self.open_img, text="Load image")
         label_a.pack(side=tk.LEFT)
 
         frame_b = tk.Frame()
         label_b = tk.Button(master=frame_b, command=self.process_img, text="Recognize plate")
         label_b.pack(side=tk.LEFT)
 
-        frame_a.pack()
+        self.frame_a.pack()
         frame_b.pack()
 
         self.window.mainloop()
